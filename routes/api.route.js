c9287889a603684ee0c094cff2dad67816a039da
max <tammanoon@dek-d.com>
2020-06-01 12:42:10 +0700

    change bot api route to post

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,15 +1,15 @@
-import express from "express";
+import express from 'express'
 import DoscgController from '../controllers/doscg.controller'
 
-const router = express.Router()
-const doscgController = new DoscgController();
+const router          = express.Router()
+const doscgController = new DoscgController()
 
-router.post('/compute/series', doscgController.computeSeries);
+router.post('/compute/series', doscgController.computeSeries)
 
-router.post('/compute/bc', doscgController.computeBC);
+router.post('/compute/bc', doscgController.computeBC)
 
-router.get('/map/bpt/cw', doscgController.getBestPathToCW);
+router.get('/map/bpt/cw', doscgController.getBestPathToCW)
 
-router.get('/line/noanswer', doscgController.getNotificationWhenNoAnswer);
+router.post('/line/noanswer', doscgController.getNotificationWhenNoAnswer)
 
-export default router;
+export default router
